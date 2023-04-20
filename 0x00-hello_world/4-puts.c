605896de6e8be4068c37ff53a5694cd8345280d4
VerahMabasa <veramabasa@gmail.com>
2023-04-20 15:09:41 -0700

    Hello, puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the puts function
- * Return 0 (Success)
- * /
- int main(void)
- {
- puts("\" Programming is like building a multilingual puzzle");
- return (0);
- }
-
+ * main - A program that prints prints a line using puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
