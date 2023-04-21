18ed73f72c0a83e68768cdba71d4413583a66740
VerahMabasa <veramabasa@gmail.com>
2023-04-21 16:07:03 -0700

    Numbers task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This program will print numbers of base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
