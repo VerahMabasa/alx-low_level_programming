1285e1a5efb6fa9be009e6be7ba7b29469b4d505
VerahMabasa <veramabasa@gmail.com>
2023-04-21 19:24:47 -0700

    100-print_comb3.c task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,22 +6,20 @@
  */
 int main(void)
 {
-	int n;
-	int m;
+	int n, m;
 
-	for (n = 48; n <= 56; n++)
+	for (n = '0'; n < '9'; n++)
 	{
-		for (m = 49; m <= 57; m++)
+	for (m = n + 1; m <= '9'; m++)
 	{
-		if (m > n)
-		{
-			putchar(n);
-			putchar(m);
-		if (n != 56 || m != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+	if (m != n)
+	{
+		putchar(n);
+		putchar(m);
+	if (n == '8' && m == '9')
+		continue;
+		putchar(',');
+		putchar(' ');
 	}
 	}
 	}
