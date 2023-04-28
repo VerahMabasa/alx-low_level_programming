258ac9cd1351aadb3bc180b15691deddcf8af688
VerahMabasa <veramabasa@gmail.com>
2023-04-28 16:26:58 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * print_numbers - print 0-9
- * return: void
+ * 3-3-3-print_numbers - print 0 to 9
+ * Return: Always 0
  */
 void print_numbers(void)
 
