3f27a083bb3f89bf5d2588c65a38aedaf800e0f8
VerahMabasa <veramabasa@gmail.com>
2023-05-02 15:31:41 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_rev - Function that prints a string, in reverse
+* @s: String reference pointer used
+* Return: Always 0
+*/
+void print_rev(char *s)
+{
+	int begin = 0;
+
+	while (s[begin])
+	begin++;
+	while (begin--)
+	_putchar(s[begin]);
+	_putchar('\n');
+}
