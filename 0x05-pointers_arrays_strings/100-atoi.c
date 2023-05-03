6540f84b61b52ed097d89b38107ff2eb7d609a1f
VerahMabasa <veramabasa@gmail.com>
2023-05-03 12:48:22 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,35 +7,29 @@
 
 int _atoi(char *s)
 {
-	int i, m, n, len, o, digit;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	i = 0;
-	m = 0;
-	n = 0;
-	len = 0;
-	o = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-	while (i < len && o == 0)
+	while (s[c])
 	{
-		if (s[i] == '.')
-			++m;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (m % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			o = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			o = 0;
-		}
-		i++;
+	if (s[c] == 45)
+	{
+	min *= -1;
 	}
-	if (o == 0)
-		return (0);
-	return (n);
+	while (s[c] >= 48 && s[c] <= 57)
+	{
+	isi = 1;
+	ni = (ni * 10) + (s[c] - '0');
+	c++;
+	}
+	if (isi == 1)
+	{
+	break;
+	}
+	c++;
+	}
+	ni *= min;
+	return (ni);
 }
