126f51425e126c91b02daea123d1b058fc3bc378
VerahMabasa <veramabasa@gmail.com>
2023-05-06 04:27:03 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of the even-valued terms
+ * not exceeding 4000000
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float tot_sum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2 == 0)
+			tot_sum += sum;
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%.0f\n", tot_sum);
+
+	return (0);
+}
