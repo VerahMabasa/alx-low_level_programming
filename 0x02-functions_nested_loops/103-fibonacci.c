607468e4973fe1c5ccf5c14a31507ec2b607f9f8
VerahMabasa <veramabasa@gmail.com>
2023-05-06 04:43:26 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,20 +7,20 @@
 
 int main(void)
 {
-	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1 = 0, fib2 = 1,fibsum;
 	float tot_sum;
 
 	while (1)
 	{
-		sum = fib1 + fib2;
-		if (sum > 4000000)
+		fibsum = fib1 + fib2;
+		if (fibsum > 4000000)
 			break;
 
-		if ((sum % 2 == 0)
-			tot_sum += sum;
+		if ((fibsum % 2) == 0)
+			tot_sum += fibsum;
 
 		fib1 = fib2;
-		fib2 = sum;
+		fib2 = fibsum;
 	}
 	printf("%.0f\n", tot_sum);
 
